48fd5246ce26d3cdef2b48bcb421541028beaac9
Samyak Maharjan <samyakjyapu33127@gmail.com>
2021-03-09 20:48:36 +0545

    implement google maps api

--- map/src/app.ts
+++ map/src/app.ts
@@ -1,10 +1,35 @@
+import CONFIG from "../config.json";
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = CONFIG.KEY;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  console.log(enteredAddress);
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.error(error);
+    });
 }
 form.addEventListener("submit", searchAddressHandler);
